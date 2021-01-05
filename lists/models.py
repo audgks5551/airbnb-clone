36a9fd5deb1ee482ca06baf9da29119b93813ba7
audgks5551 <70470019+audgks5551@users.noreply.github.com>
2021-01-05 10:57:58 +0900

    lists app

--- lists/models.py
+++ lists/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from core import models as core_models
+
+
+class List(core_models.TimeStampedModel):
+
+    """ List Model Definition """
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
